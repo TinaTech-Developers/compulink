8d8cdda02c703150f38d8a02f1f75c642921db8a
TinaTech-Developers <tinashephiri0@gmail.com>
2024-03-25 12:45:58 +0200

    removed the secret keys new

--- src/app/apply/[id]/page.js
+++ src/app/apply/[id]/page.js
@@ -1,50 +1,54 @@
 import ApplicationForm from "@/components/ApplicationForm";
 import React from "react";
 
-// export async function generateStaticParams() {
-//   const response = await fetch("http://localhost:3000/api/vaccant/");
-//   const vaccant = await response.json();
-//   return vaccant.vaccant.map((vac) => ({
-//     id: vac._id.toString(),
-//   }));
-// }
+export async function generateStaticParams() {
+  const response = await fetch("http://localhost:3000/api/vaccant/");
+  const vaccant = await response.json();
+  return vaccant.vaccant.map((vac) => ({
+    id: vac._id.toString(),
+  }));
+}
 
-// async function getVaccants(id) {
-//   const res = await fetch(`http://localhost:3000/api/vaccant/${id}`);
-//   const vaccant = await res.json();
-//   return vaccant;
-// }
-const getApplicationById = async (id) => {
-  try {
-    const res = await fetch(`http://localhost:3000/api/vaccant/${id}`, {
-      cache: "no-store",
-    });
+async function getVaccants(id) {
+  const res = await fetch(`http://localhost:3000/api/vaccant/${id}`);
+  const vaccant = await res.json();
+  return vaccant;
+}
+// const getApplicationById = async (id) => {
+//   try {
+//     const res = await fetch(`http://localhost:3000/api/vaccant/${id}`, {
+//       cache: "no-store",
+//     });
 
-    if (!res.ok) {
-      throw new Error("Unable to continue");
-    }
+//     if (!res.ok) {
+//       throw new Error("Unable to continue");
+//     }
 
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     return res.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 export default async function Application({ params }) {
   const { id } = params;
-  const { vaccant } = await getApplicationById(id);
-  const {
-    jobTitle,
-    branch,
-    duties,
-    qualifications,
-    contract,
-    salary,
-    department,
-  } = vaccant;
+  try {
+    const { vaccant } = await getVaccants(id);
+    if (!vaccant) {
+      return <div>No data available for this vacancy.</div>;
+    }
 
-  return (
-    <>
+    const {
+      jobTitle,
+      branch,
+      duties,
+      qualifications,
+      contract,
+      salary,
+      department,
+    } = vaccant;
+
+    return (
       <ApplicationForm
         id={id}
         jobTitle={jobTitle}
@@ -55,6 +59,10 @@ export default async function Application({ params }) {
         salary={salary}
         department={department}
       />
-    </>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    // Handle the error (e.g., show an error message)
+    return <div>Error fetching data. Please try again later.</div>;
+  }
 }
