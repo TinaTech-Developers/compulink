e2144c18031ff6c897ee5214a148f39d7ac827e3
TinaTech-Developers <tinashephiri0@gmail.com>
2024-04-11 12:14:57 +0200

    added calbackUrl

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,27 @@
 "use client";
-import { signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
+
+import { signOut, useSession } from "next-auth/react";
 import React from "react";
 
 function Header() {
-  const start = Date.toLocaleString();
+  const signOutUrl = "/dashboard";
   const { data: session } = useSession();
   return (
     <div className="flex justify-between px-4 py-6 w-full border-b-2 border-white bg-gray-100  ">
       <h1 className="font-bold">Dashboard</h1>
-      <span className="text-gray-300" suppressHydrationWarning>
-        {Date(start)}
-      </span>
 
       <div className="flex flex-col items-center ">
         <h1>
           Welcome Back, <span className="font-bold">{session?.user?.name}</span>
         </h1>
-        <Link href={"/dashboard"}>
-          <button
-            onClick={() => signOut()}
-            className="text-red-400 text-sm hover:text-red-800"
-          >
-            SignOut
-          </button>
-        </Link>
+        {/* <Link href={"/dashboard"}> */}
+        <button
+          onClick={() => signOut({ callbackUrl: signOutUrl })}
+          className="text-red-400 text-sm hover:text-red-800"
+        >
+          SignOut
+        </button>
+        {/* </Link> */}
       </div>
     </div>
   );
