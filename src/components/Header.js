3d10368436d77553c157d5bbdc00d42a84a890db
TinaTech-Developers <tinashephiri0@gmail.com>
2024-04-15 11:29:02 +0200

    dashboard navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,18 @@ function Header() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex justify-between px-4 py-6 w-full border-b-2 border-white bg-gray-100  ">
+    <div className="grid md:flex md:justify-between gap-4 px-4 py-6 w-full border-b-2 border-white bg-gray-100  ">
       <h1 className="font-bold">Dashboard</h1>
 
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col  ">
         <h1>
           Welcome Back,
-          <span className="font-bold">{session?.user?.email}</span>
+          <span className="font-bold"> {session?.user?.email}</span>
         </h1>
 
         <button
           onClick={() => signOut({ callbackUrl: signOutUrl })}
-          className="text-red-400 text-sm hover:text-red-800"
+          className="text-red-400 text-sm hover:text-red-800 py-4"
         >
           SignOut
         </button>
